db800ff60d74bf911d8b0f4baa5cba6b3a171ab6
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-11 23:14:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,5 +11,11 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer ()
   
 # Write your code here.
