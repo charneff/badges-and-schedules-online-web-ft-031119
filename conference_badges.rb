e3fc81f1608bfedea5dfaecd4442ad93884df62f
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-11 23:11:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+badge_creator_method(array)
 # Write your code here.
